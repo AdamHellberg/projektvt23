67e590f55dfe601b71e9b48a9d89687ec2e80b63
AdamHellberg <adam.hellberg@elev.ga.ntig.se>
2023-03-23 09:37:38 +0100

    oj jag glömde pusha
    jag har gjort färdigt den första sidan och har har nu börjat att anpassa det till mobilen

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 // Denna fil skall innehålla JavaScript (JS)
+
+const ham = document.querySelector("#ham");
+const meny = document.querySelector("#meny");
+const invis = document.querySelector(".invis");
+const sok = document.querySelector("#sok");
+
+ham.addEventListener('click', toggleMenu )
+
+function toggleMenu(event) {
+    event.preventDefault();
+    meny.classList.toggle("active");
+    invis.classList.toggle("active");
+    sok.classList.toggle("active");
+  }
